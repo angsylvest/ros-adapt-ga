3a8d8453995e928507aa5a43a5f032681462f26f
angsylvest <sylve057@umn.edu>
2023-06-13 11:49:29 -0500

    preliminary work using real-robots

--- init_pkg/src/init_pose.py
+++ init_pkg/src/init_pose.py
@@ -3,6 +3,8 @@
 import rospy
 from geometry_msgs.msg import PoseWithCovarianceStamped
 from nav_msgs.msg import Odometry
+from std_msgs.msg import Float32 
+
 
 # Node initialization
 rospy.init_node('init_pose')
@@ -11,6 +13,16 @@ pub = rospy.Publisher('/initialpose', PoseWithCovarianceStamped, queue_size = 1)
 # Construct message
 init_msg = PoseWithCovarianceStamped()
 init_msg.header.frame_id = "map"
+
+sense_info = ""
+
+def process_info(data):
+    global sense_info 
+
+    sense_info = data
+
+pop_subscriber = rospy.Subscriber('light_pub_one', Float32, process_info)
+
 
 # Get initial pose from Gazebo
 odom_msg = rospy.wait_for_message('/odom', Odometry)
@@ -28,3 +40,6 @@ rospy.sleep(1)
 rospy.loginfo("setting initial pose")
 pub.publish(init_msg)
 rospy.loginfo("initial pose is set")
+
+while not rospy.is_shutdown():
+    print(sense_info)
