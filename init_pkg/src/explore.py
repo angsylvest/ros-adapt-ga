7001deda4b495134509f2e4885907f905c427231
angsylvest <sylve057@umn.edu>
2024-12-02 15:45:34 -0600

    general refactoring

--- init_pkg/src/explore.py
+++
@@ -1,4 +0,0 @@
-# generate random strategy (single agent to start)
-
-# have robot proceed until completion 
-    # use odom and path length to determine straight line distance and where to orient towards
